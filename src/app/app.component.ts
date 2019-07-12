6cfbebe988bf977e13d6b109f1aaefc6e04c92a4
PGboy <zscfde@gmail.com>
2019-07-12 18:20:38 +0800

    feat: make a simple send mail page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'simple-transactional-mail';
+  constructor(public afAuth: AngularFireAuth, private fun: AngularFireFunctions) { }
+
+  loginWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    this.afAuth.auth.signInWithPopup(provider);
+  }
+
+  sendEmail() {
+    const callable = this.fun.httpsCallable('genericEmail');
+    callable({
+      subject: 'Email from Angular',
+      name: 'XXX',
+      text: 'Sending email with Angular and SendGrid is fun!'
+    }).subscribe(res => {
+      console.log('res', res);
+    });
+  }
 }
